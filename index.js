94d806ab74b7c17e65f6bcdc07819d252a9a5748
Claire Price <claire.price@mail.com>
2024-02-02 13:48:50 +0000

    add questions and inquirer.prompt

--- index.js
+++ index.js
@@ -1,21 +1,94 @@
+// const fs = require("fs");
+// const path = require('path');
+// const inquirer = require("inquirer");
+// const generateMarkdown = require("./utils/generateMarkdown");
+
+// // array of questions for user
+// const questions = [
+
+// ];
+
+// // function to write README file
+// function writeToFile(fileName, data) {
+// }
+
+// // function to initialize program
+// function init() {
+
+// }
+
+// // function call to initialize program
+// init();
+
+
 const fs = require("fs");
 const path = require('path');
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
+const util = require('util');
 
 // array of questions for user
 const questions = [
-
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Project title',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Project description',
+    },
+    {
+        type: 'input',
+        name: 'contents',
+        message: 'Table of Contents',
+    },
+    {
+        type: 'input',
+        name: 'installtion',
+        message: 'Installation Instructions',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Usage Instructions',
+    },
+    {
+        type: 'input',
+        name: 'license',
+        message: 'License Information',
+    },
+    {
+        type: 'input',
+        name: 'contributors',
+        message: 'Who has contributed?',
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Tests',
+    },
+    {
+        type: 'input',
+        name: 'questions',
+        message: 'Questions',
+    },
 ];
 
-// function to write README file
-function writeToFile(fileName, data) {
-}
+inquirer.prompt(questions);
 
-// function to initialize program
-function init() {
 
-}
+// const writeToFilePromise = util.promisify(fs.writeFile);
+
+// async function writeToFile(data) {
+//     try {
+//         await writeToFilePromise('README.md', generateMarkdown(data));
+//         console.log('File written successfully\n');
+//     } catch (err) {
+//         console.log('Error writing to file', err);
+//     }
+// }
 
 // function call to initialize program
-init();
+// writeToFile();
