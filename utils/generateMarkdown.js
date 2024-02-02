ad7f67a66afb1ec3a941ea46c74d45e310f94159
Claire Price <claire.price@mail.com>
2024-02-02 21:39:12 +0000

    add basic markdown structure

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,30 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `# ${data.title} 
 
+  ## Description
+  ${data.description} 
+
+  ## Table of Contents
+  ${data.contents} 
+
+  ## Installation Instructions
+  ${data.installation} 
+
+  ## Usage
+  ${data.usage}
+
+  ## License
+  ${data.license}
+
+  ## Contributors
+  ${data.contributors}
+
+  ## Tests
+  ${data.tests}
+
+  ## Questions
+  If you have any questions about the repo, open an issue or contact me directly at ${data.email}. You can find more of my work at ${data.github}
 `;
 }
 
