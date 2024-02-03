da27381950d4db3666a214f7f103306c6c4d20fd
Claire Price <claire.price@mail.com>
2024-02-03 23:45:40 +0000

    placeholder links work for badge

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,16 @@
 // function to generate markdown for README
-function generateMarkdown(data) {
+function generateMarkdown(data, ) {
   const { title, description, installInstruction, installCommand, usage, license, contributors, testInstruction, testCommand, email, github } = data;
+  
+  const licenses = ['MIT', 'Apache License 2.0', 'GNU General Public License v2.0', 'GNU General Public License v3.0'];
+  const links = ['one', 'two', 'three', 'four']
+  const licensesIndex = licenses.indexOf(license.toString());
+  const badge = links[licensesIndex];
+
+
 
   return `# ${title} 
+  ${badge};
 
   ## Description
   ${description} 
@@ -38,5 +46,8 @@ function generateMarkdown(data) {
   If you have any questions about the repo, open an issue or contact me directly at ${email}. You can find more of my work at ${github}
 `;
 }
+
+
+
 
 module.exports = generateMarkdown;
