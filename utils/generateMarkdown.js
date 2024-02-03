ed2cc071582cd0f5ae05cbea855b1c568df89206
Claire Price <claire.price@mail.com>
2024-02-03 23:54:13 +0000

    add correct links for badges in md

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -3,14 +3,16 @@ function generateMarkdown(data, ) {
   const { title, description, installInstruction, installCommand, usage, license, contributors, testInstruction, testCommand, email, github } = data;
   
   const licenses = ['MIT', 'Apache License 2.0', 'GNU General Public License v2.0', 'GNU General Public License v3.0'];
-  const links = ['one', 'two', 'three', 'four']
   const licensesIndex = licenses.indexOf(license.toString());
+  const links = [
+    '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)', 
+    '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)', 
+    '[![License: GPL v2](https://img.shields.io/badge/License-GPL_v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)', 
+    '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)']
   const badge = links[licensesIndex];
 
-
-
   return `# ${title} 
-  ${badge};
+  ${badge}
 
   ## Description
   ${description} 
